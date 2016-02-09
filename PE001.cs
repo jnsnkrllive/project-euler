a69027ed1c3360ce928994b521c0f0f59805a654
Karl Jansen <jnsnkrl@live.com>
2016-02-08 19:23:59 -0800

    solution 1

---
+++ PE001.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectEuler {
+
+    class PE001 {
+
+        static void Main(string[] args) {
+            System.Console.WriteLine("Problem 1:");
+            System.Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.");
+            System.Console.WriteLine();
+
+            int highNumber = 1000;
+            int sum = 0;
+            for (int i = 0; i < highNumber; i++) {
+                if (i % 3 == 0 || i % 5 == 0) {
+                    sum += i;
+                }
+            }
+
+            System.Console.WriteLine("The sum of the multples of 3 or 5 up to {0} is {1}", highNumber, sum);
+        }
+
+    }
+
+}
