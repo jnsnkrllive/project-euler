cf81350159e72129b6122d3d5186b90a84e80dfa
Karl Jansen <jnsnkrl@live.com>
2016-02-08 19:30:53 -0800

    fix problem statement formatting

--- PE001.cs
+++ PE001.cs
@@ -4,7 +4,8 @@
 
         static void Main(string[] args) {
             System.Console.WriteLine("Problem 1:");
-            System.Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.");
+            System.Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.");
+            System.Console.WriteLine("Find the sum of all the multiples of 3 or 5 below 1000.");
             System.Console.WriteLine();
 
             int highNumber = 1000;
