5e95055cfdc08b9d0dc7abf4fb456259bc68212d
Karl Jansen <jnsnkrl@live.com>
2016-02-09 21:00:48 -0800

    start solution 5

---
+++ PE005.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectEuler {
+
+    class PE005 {
+
+        public static void execute() {
+            System.Console.WriteLine("Problem 5:");
+            System.Console.WriteLine("2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.");
+            System.Console.WriteLine("What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?");
+            System.Console.WriteLine();
+
+            int smallestMultiple = -1;
+            // TODO
+
+            System.Console.WriteLine("The smallest positive number evenly divisible by all numbers from 1 to 20 is {0}", smallestMultiple);
+        }
+
+    }
+
+}
