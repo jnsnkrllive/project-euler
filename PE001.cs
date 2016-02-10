ad5ac28a8fb8116a32f3d46f5896738812bad830
Karl Jansen <jnsnkrl@live.com>
2016-02-09 18:06:02 -0800

    fix to use with solution runner

--- PE001.cs
+++ PE001.cs
@@ -2,7 +2,7 @@
 
     class PE001 {
 
-        static void Main(string[] args) {
+        public static void execute() {
             System.Console.WriteLine("Problem 1:");
             System.Console.WriteLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.");
             System.Console.WriteLine("Find the sum of all the multiples of 3 or 5 below 1000.");
