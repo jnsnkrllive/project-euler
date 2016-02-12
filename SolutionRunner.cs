895fab6775f27534508c20e303444fad5497e7ec
Karl Jansen <jnsnkrl@live.com>
2016-02-12 15:38:32 -0800

    solution 11 (Run Time: 00:00:00.007)

--- SolutionRunner.cs
+++ SolutionRunner.cs
@@ -12,10 +12,10 @@ namespace ProjectEuler {
         static void Main(string[] args) {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            PE010.execute();
+            PE011.execute();
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:000}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
             Console.WriteLine("Run Time: {0}", elapsedTime);
         }
 
