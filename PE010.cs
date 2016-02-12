de1570b6b60a0b6654ba24e8e3444894f9cffce1
Karl Jansen <jnsnkrl@live.com>
2016-02-11 21:46:59 -0800

    solution 10 (Run Time: 00:08:56.78)

---
+++ PE010.cs
@@ -0,0 +1,60 @@
+﻿namespace ProjectEuler {
+
+    class PE010 {
+
+        private static int FIRST_PRIME = 2;
+
+        public static void execute() {
+            System.Console.WriteLine("Problem 10:");
+            System.Console.WriteLine("The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.");
+            System.Console.WriteLine("Find the sum of all the primes below two million.");
+            System.Console.WriteLine();
+
+            int boundUpper = 2000000;
+            bool[] primes = new bool[boundUpper];
+            primes[0] = false;
+            primes[1] = false;
+            primes[FIRST_PRIME] = true;
+            for (int i = FIRST_PRIME; i < primes.Length; i++) {
+                primes[i] = true;
+            }
+            for (int i = FIRST_PRIME; i < primes.Length; i++) {
+                if (!primes[i]) {
+                    continue;
+                }
+                if (isPrime(i, primes)) {
+                    primes[i] = true;
+                    for (int j = i + i; j < primes.Length; j = j + i) {
+                        primes[j] = false;
+                    }
+                }
+            }
+            long sum = 0;
+            for (int i = 0; i < primes.Length; i++) {
+                if (primes[i]) {
+                    sum += i;
+                }
+            }
+
+            System.Console.WriteLine("Answer is {0}", sum);
+        }
+
+        /**
+         * Returns TRUE if the given number is prime
+         */
+        private static bool isPrime(int n, bool[] primes) {
+            for (int i = FIRST_PRIME; i < n/2; i++) {
+                if (!primes[i]) {
+                    // 'i' is not prime, so skip it
+                    continue;
+                }
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+
+}
--- SolutionRunner.cs
+++ SolutionRunner.cs
@@ -12,11 +12,11 @@ namespace ProjectEuler {
         static void Main(string[] args) {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            PE009.execute();
+            PE010.execute();
             stopWatch.Stop();
             TimeSpan ts = stopWatch.Elapsed;
             string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-            Console.WriteLine("RunTime: {0}", elapsedTime);
+            Console.WriteLine("Run Time: {0}", elapsedTime);
         }
 
     }
