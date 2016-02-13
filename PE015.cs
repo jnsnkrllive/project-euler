db6b82b95f87fc91dbbc5f5f369bc25ebe543f71
Karl Jansen <jnsnkrl@live.com>
2016-02-12 18:26:17 -0800

    solution 15 (Run Time: 00:00:00.002)

---
+++ PE015.cs
@@ -0,0 +1,51 @@
+﻿namespace ProjectEuler {
+
+    class PE015 {
+
+        private static byte GRID_SIZE = 20;
+        private static byte MATRIX_SIZE = (byte)(GRID_SIZE + 1);
+        private static long[][] MATRIX = new long[MATRIX_SIZE][];
+
+        public static void execute() {
+            printProblemStatement();
+
+            for (byte i = 0; i < MATRIX.Length; i++) {
+                MATRIX[i] = new long[MATRIX_SIZE];
+            }
+
+            long answer = pathsToEndFromPoint(0, 0);
+
+            System.Console.WriteLine("Answer is {0}", answer);
+        }
+
+        private static long pathsToEndFromPoint(byte i, byte j) {
+            if (MATRIX[i][j] != 0) {
+                return MATRIX[i][j];
+            }
+            long pathsToEnd = 0;
+            if (i == MATRIX_SIZE - 1 && j == MATRIX_SIZE - 1) {
+                pathsToEnd = 1;
+            }
+            else if (i == MATRIX_SIZE - 1 && j != MATRIX_SIZE - 1) {
+                pathsToEnd = 1;
+            }
+            else if (i != MATRIX_SIZE - 1 && j == MATRIX_SIZE - 1) {
+                pathsToEnd = 1;
+            }
+            else {
+                pathsToEnd = pathsToEndFromPoint((byte)(i + 1), j) + pathsToEndFromPoint(i, (byte)(j + 1));
+            }
+            MATRIX[i][j] = pathsToEnd;
+            return pathsToEnd;
+        }
+
+        private static void printProblemStatement() {
+            System.Console.WriteLine("Problem 15:");
+            System.Console.WriteLine("Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.");
+            System.Console.WriteLine("How many such routes are there through a 20×20 grid?");
+            System.Console.WriteLine();
+        }
+
+    }
+
+}
