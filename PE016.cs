43a930620993c97b3b3c1302d88c9d8a7a8ce808
Karl Jansen <jnsnkrl@live.com>
2016-02-12 19:02:35 -0800

    solution 16 (Run Time: 00:00:00.012)

---
+++ PE016.cs
@@ -0,0 +1,44 @@
+﻿namespace ProjectEuler {
+
+    class PE016 {
+
+        public static void execute() {
+            printProblemStatement();
+            
+            int[] digits = new int[2 * System.Byte.MaxValue];
+            digits[0] = 2;
+            int targetExp = 1000;
+            for (int i = 1; i < targetExp; i++) {
+                int carryover = 0;
+                for (int j = 0; j < digits.Length; j++) {
+                    int newNumber = 2 * digits[j];
+                    if (carryover > 0) {
+                        digits[j] = newNumber % 10 + carryover;
+                        carryover = 0;
+                    }
+                    else {
+                        digits[j] = newNumber % 10;
+                    }
+                    if (newNumber > 9) {
+                        carryover = newNumber / 10;
+                    }
+                }
+            }
+            int sum = 0;
+            for (int i = 0; i < digits.Length; i++) {
+                sum += digits[i];
+            }
+
+            System.Console.WriteLine("Answer is {0}", sum);
+        }
+
+        private static void printProblemStatement() {
+            System.Console.WriteLine("Problem 16:");
+            System.Console.WriteLine("2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.");
+            System.Console.WriteLine("What is the sum of the digits of the number 2^1000?");
+            System.Console.WriteLine();
+        }
+
+    }
+
+}
