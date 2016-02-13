7fb517dbf6ee2a13734887c187b020a58766dc00
Karl Jansen <jnsnkrl@live.com>
2016-02-12 19:08:10 -0800

    start solution 17

---
+++ PE017.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectEuler {
+
+    class PE017 {
+
+        public static void execute() {
+            printProblemStatement();
+
+            // TODO
+            int answer = -1;
+
+            System.Console.WriteLine("Answer is {0}", answer);
+        }
+
+        private static void printProblemStatement() {
+            System.Console.WriteLine("Problem 17:");
+            System.Console.WriteLine("If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.");
+            System.Console.WriteLine("If all the numbers from 1 to 1000(one thousand) inclusive were written out in words, how many letters would be used?");
+            System.Console.WriteLine("NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of \"and\" when writing out numbers is in compliance with British usage.");
+            System.Console.WriteLine();
+        }
+
+    }
+
+}
