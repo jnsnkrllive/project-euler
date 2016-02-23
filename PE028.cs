f35141210e9bc261f8e17b75717250b0f88c7590
Karl Jansen <jnsnkrl@live.com>
2016-02-22 22:54:04 -0800

    solution 28 (Run Time 00:00:00.000)

---
+++ PE028.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProjectEuler {
+
+    class PE028 : PESolution {
+
+        void PESolution.printProblemNumber() {
+            Console.WriteLine("Problem 28:");
+        }
+
+        void PESolution.printProblemStatement() {
+            Console.WriteLine("Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:");
+            Console.WriteLine("21 22 23 24 25");
+            Console.WriteLine("20  7  8  9 10");
+            Console.WriteLine("19  6  1  2 11");
+            Console.WriteLine("18  5  4  3 12");
+            Console.WriteLine("17 16 15 14 13");
+            Console.WriteLine("It can be verified that the sum of the numbers on the diagonals is 101.");
+            Console.WriteLine("What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?");
+        }
+
+        void PESolution.solve() {
+            int sum = 1;
+            int count = 1;
+            int spiralSize = 1001;
+            for (int i = 2; i <= (spiralSize / 2 + 1); i++) {
+                for (int j = 1; j <= 4; j++) {
+                    count += (2 * i - 2);
+                    sum += count;
+                }
+            }
+            Console.WriteLine("Answer is {0}", sum);
+        }
+
+    }
+
+}
--- SolutionRunner.cs
+++ SolutionRunner.cs
@@ -10,7 +10,7 @@ namespace ProjectEuler {
     class SolutionRunner {
 
         static void Main(string[] args) {
-            executeSingle("027");
+            executeSingle("028");
             //executeAll();
         }
 
@@ -47,7 +47,7 @@ namespace ProjectEuler {
         private static String[] SOLUTIONS = new String[] {
                    "001", "002", "003", "004", "005", "006", "007", "008", "009",
             "010", "011", "012", "013", "014", "015", "016", "017", "018", "019",
-            "020", "021", "022", "023", "024", "025", "026", "027",
+            "020", "021", "022", "023", "024", "025", "026", "027", "028",
             "067",
         };
 
