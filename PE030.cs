0bda2cda44053da2186bc9ad4e43b8ec8392b911
Karl Jansen <jnsnkrl@live.com>
2016-02-23 22:27:40 -0800

    solution 30 (Run Time: 00:00:00.189)

---
+++ PE030.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ProjectEuler {
+
+    class PE030 : PESolution {
+
+        void PESolution.printProblemNumber() {
+            Console.WriteLine("Problem 30:");
+        }
+
+        void PESolution.printProblemStatement() {
+            Console.WriteLine("Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.");
+        }
+
+        void PESolution.solve() {
+            int[] fifths = new int[10];
+            for (int i = 0; i < fifths.Length; i++) {
+                fifths[i] = Convert.ToInt32(Math.Pow(i, 5));
+            }
+            int total = 0;
+            int boundUpper = 355000;
+            // Brute Force
+            for (int i = 2; i < boundUpper; i++) {
+                char[] number = i.ToString().ToCharArray();
+                int sum = 0;
+                for (int j = 0; j < number.Length; j++) {
+                    sum += fifths[Convert.ToByte(char.GetNumericValue(number[j]))];
+                }
+                if (sum == i) {
+                    total += i;
+                }
+            }
+
+            Console.WriteLine("Answer is {0}", total);
+        }
+
+    }
+
+}
